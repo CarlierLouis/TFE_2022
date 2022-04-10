b920783e7565cabeac01a07d2bea61c417c0cd66
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-04-10 16:22:50 +0200

    Nouvelle animation sur le portail de redirections

--- client/src/common/portal/Portal.js
+++ client/src/common/portal/Portal.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardGroup } from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
 
 import './Portal.css';
 
 const Portal  = () => {
     return (
 <CardGroup className="portal">
-  <Card className="portal-GH" style={{ backgroundColor: 'rgb(31, 30, 30)'}} >
-    <Card.Img variant="top" src="img/Grand-Hallet-photo_1.jpg" />
+  <Card className="portal-GH border-0" style={{ backgroundColor: 'rgb(31, 30, 30)'}}>
     <Link to="/grand-hallet">
+  <div className="animated-portal-div">
+    <span>
+      <Card.Img variant="top" src="img/Grand-Hallet-photo_1.jpg" />
+      <div>
       <img className='txt-GH' src="img/Grand-Hallet_blanc.png" ></img>
-    </Link>
+      </div>
+    </span>
+  </div>
+  </Link>
+    
   </Card>
-  <Card className="portal-Moxhe" style={{ backgroundColor: 'rgb(31, 30, 30)'}} >
+
+  <Card className="portal-Moxhe border-0" style={{ backgroundColor: 'rgb(31, 30, 30)'}} >
+    <Link to="/moxhe">
+  <div className="animated-portal-div">
+    <span>
     <Card.Img variant="top" src="img/Moxhe-photo_1.jpg" />
-      <Link to="/moxhe">
+      <div>
         <img className='txt-Moxhe' src="img/Moxhe_blanc.png"></img>
-    </Link>
+    </div>
+    </span>
+  </div>
+  </Link>
+
   </Card>
+  
 </CardGroup>
+
+
   )
 }
 
