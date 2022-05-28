fb706ee71a430f0718c29c9c40e3ccebc83558ad
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-05-28 16:02:17 +0200

    Ajout des svg pour modification ou suppression d'un utilisateurs (whitelist compris)

--- client/src/admin/UserItem.js
+++ client/src/admin/UserItem.js
@@ -4,15 +4,17 @@ import Card from "../common/UIElements/Card";
 import './UserItem.css';
 
 const UserItem = props => {
-    
+
 
     return (
         <React.Fragment>
             <div className="user-item center">
                 <Card className="user-item-card">
+                    <img className="user-modify" src="/svg/modify-red.svg"/>
                     <p className="user-name">{props.name}</p>
                     <p className="user-firstname">{props.firstname}</p>
                     <p className="user-email">{props.email}</p>
+                    <img className="user-delete" src="/svg/delete-red.svg"/>
                     <p className="user-role">{props.role}</p>
                     <p className="user-classyear">{props.classyear}</p>
                 </Card>
