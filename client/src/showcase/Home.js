c46a76b0686037a0833e989ba8109d443a70f120
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-06-03 17:10:53 +0200

    Qq ajustements css

--- client/src/showcase/Home.js
+++ client/src/showcase/Home.js
@@ -54,12 +54,7 @@ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mau
 Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. 
 Mauris ullamcorper felis vitae erat. 
 Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.
-Aliquam convallis sollicitudin purus. 
-Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. 
-Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat imperdiet. 
-Aenean suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. 
-Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus. 
-Curabitur eu amet. »</elem></Card><br></br>
+Aliquam convallis sollicitudin purus</elem></Card><br></br>
 
 <div className='home-youtube-video-div'>
     <iframe className='home-youtube-video'
