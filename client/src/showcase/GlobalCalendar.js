d6f0622fa5e46e22da3f9cbe298f5e65efdcdee1
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-07-13 12:44:06 +0200

    Suite agenda global partie admin

--- client/src/showcase/GlobalCalendar.js
+++ client/src/showcase/GlobalCalendar.js
@@ -12,6 +12,8 @@ import  { useHttpClient } from '../common/hooks/http-hook';
 import { AuthContext } from '../common/context/auth-context';
 import ErrorModal from '../common/UIElements/ErrorModal';
 import LoadingSpinner from '../common/UIElements/LoadingSpinner';
+import Modal from "../common/UIElements/Modal";
+import Button from "../common/FormElements/Button";
 
 import './GlobalCalendar.css';
 import '../admin/GlobalCalendar/GlobalCalendar.css';
@@ -34,10 +36,13 @@ const GlobalCalendar = props => {
     const [loadedCalendar, setLoadedCalendar] = useState();
     const {isLoading, error, sendRequest, clearError} = useHttpClient();
     const school = useParams().school;
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+    const [loadedEvents, setLoadedEvents] = useState();
 
     useEffect(() => {
         const fetchcalendar = async () => {
-            const events = []
+            const events = [];
             try {
             const responseData = await sendRequest(
                 process.env.REACT_APP_BACKEND_URL + `/api/calendar/${school}`, 'GET', null,
@@ -51,7 +56,9 @@ const GlobalCalendar = props => {
                     end: element.end
                 }
 
-                events.push(event)
+                events.push(event);
+
+                setLoadedEvents(responseData.events);
             });
 
             setLoadedCalendar(events);
@@ -63,6 +70,45 @@ const GlobalCalendar = props => {
     }, [sendRequest]);
 
 
+    const showDeleteWarningHandler = () => {
+		setShowConfirmModal(true);
+	};
+
+	const cancelDeleteHandler = () => {
+		setShowConfirmModal(false);
+	};
+
+    const openMoreHandler = () => {
+        setShowMore(true);
+      };
+    
+    const closeMoreHandler = () => {
+      setShowMore(false);
+      };
+
+      const confirmDeleteHandler = async () => {
+		setShowConfirmModal(false);
+		try {
+			await sendRequest(
+				process.env.REACT_APP_BACKEND_URL + `/api/calendar/${props.id}`,
+				'DELETE',
+				null,
+				{Authorization: 'Bearer ' + auth.token}
+			);
+			props.onDelete(props.id);
+		}
+		catch(err) {}
+
+    if (!error) {
+    refreshPage();
+    }
+	};
+
+    const refreshPage = () =>{
+        window.location.reload(true);
+    }
+
+
     return(
         <React.Fragment>
             {school == "grand-hallet" && 
@@ -70,26 +116,39 @@ const GlobalCalendar = props => {
                                 schoolLogo="/svg/Grand-Hallet_blanc.svg" />}
 
             {school == "moxhe" && 
-            <MainNavigation schoolLink="moxhe"
+            <MainNavigation schoolLink="moxhe"   
                             schoolLogo="/svg/Moxhe_blanc.svg" />}
            
             <ErrorModal error={error} onClear={clearError} />
+
+            
+            <Modal
+            show={showMore}
+            onCancel={closeMoreHandler}
+            footer={<Button onClick={closeMoreHandler}>Fermer</Button>}>
+            
+            {loadedCalendar != undefined && 
+            <h1>{loadedEvents[1].id}</h1>}
+            </Modal>
+
+            
 
             {auth.isLoggedIn && auth.role == "Admin" && window.location.pathname != `/${school}` &&
             <a href={`/${school}/admin/ajouter-evenement-calendrier`}>
                 <img className='red-plus-add-event' src='/svg/red-plus.svg'></img>
             </a>}
 
+            {isLoading && 
+            <div className='center'>
+                <LoadingSpinner />
+            </div>}
+
             <Card className="global-calendar-Card-div">
 
                 <h2 className="global-agenda-title">Agenda général</h2>
+
                 
-                {isLoading && 
-                <div className='center'>
-                    <LoadingSpinner />
-                </div>}
-                
-                {!isLoading && 
+                {!isLoading && loadedCalendar &&
                 <Calendar 
                     localizer={localizer}
                     events={loadedCalendar}
@@ -118,6 +177,7 @@ const GlobalCalendar = props => {
                         style: { backgroundColor: start == end ? "#4FC3A1": "#324960",
                          color: "white"}
                     })}
+                    onSelectEvent={openMoreHandler}
                 />}
 
             </Card>
