0a3bb3bd1bf74759030e66bf52f90440b4b89cd2
Louis Carlier <63374771+CarlierLouis@users.noreply.github.com>
2022-07-30 14:59:20 +0200

    Suite gestion documents dans les parties persos

--- client/src/user/Documents.js
+++ client/src/user/Documents.js
@@ -4,6 +4,7 @@ import { AuthContext } from '../common/context/auth-context';
 import { useParams } from 'react-router-dom';
 import { useHttpClient } from '../common/hooks/http-hook';
 import Card from '../common/UIElements/Card';
+import Button from '../common/FormElements/Button';
 import LoadingSpinner from '../common/UIElements/LoadingSpinner';
 
 import './Documents.css';
@@ -40,6 +41,9 @@ const Documents = props => {
     return (
         <React.Fragment>
             <Card className="documents-card">
+
+            <h2 style={{textAlign: 'center'}}>Documents utiles et/ou importants</h2><br></br>
+
             {(auth.role == "Default" || auth.role == "Admin") && 
             <a href={`/${school}/espace-prof/documents/${props.classyear}/ajouter-document`}>
                 <img className='red-plus-add-document' src='/svg/red-plus.svg'></img>
@@ -50,13 +54,35 @@ const Documents = props => {
                 <LoadingSpinner />
             </div>}
 
+            {(auth.role == "Default" || auth.role == "Admin") &&
+                <div>
+                    <h4 className="documents-class-title">Classe: {props.classyear}</h4>
+                    <h5 className="documents-change-class">
+                        <a href={"/" + school + "/espace-prof" + "/documents"}>Changer de classe</a>
+                    </h5>
+                   <br></br>
+                </div>}
+
+            
+               {loadedDocuments && loadedDocuments.length == 0 &&
+                    <div className="center">
+                        <Card>
+                        <h2>Aucun document trouvé</h2>
+                        </Card>
+                    </div>
+                }
+
+
             {!isLoading && loadedDocuments && 
             <ul className="documents-list">
             {loadedDocuments.map(documents => (
                 <li>
                 <a href={process.env.REACT_APP_BACKEND_URL + `/${documents.file}`} target="_blank">
-                    {documents.title}
-                </a>
+                <Button>
+                    <img className="document-img" src="/svg/pdf.svg" />
+                    &nbsp;&nbsp;{documents.title}
+                </Button>
+                </a><br></br><br></br>
                 </li>
             ))}
             </ul>}
--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -15,7 +15,7 @@ const fileUpload = multer({
         },
         filename: (req, file, cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype];
-            cb(null, uuidv1() + "." + ext);
+            cb(null, file.originalname);
         }
     }),
     fileFilter: (req, file, cb) => {
