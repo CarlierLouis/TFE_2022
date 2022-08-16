e5fe35bfaf77169d7c4a0136790181496c19fffe
Louis Carlier <63374771+CarlierLouis@users.noreply.github.com>
2022-08-16 23:18:04 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -81,7 +81,7 @@ app.use((error, req, res, next) => {
 mongoose
 .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kerju.mongodb.net/schoolsdb?retryWrites=true&w=majority`)
 .then(() => {
-    app.listen(process.env.PORT);
+    app.listen(process.env.PORT || 5000);
 })
 .catch(err => {
     console.log(err);
