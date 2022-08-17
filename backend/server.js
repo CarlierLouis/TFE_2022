9a1da08de7928571b2ccf7a8bd73b2be459e2993
Louis Carlier <63374771+CarlierLouis@users.noreply.github.com>
2022-08-18 00:11:24 +0200

    suite préparation au déploiement

--- backend/server.js
+++ backend/server.js
@@ -79,7 +79,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kerju.mongodb.net/schoolsdb?retryWrites=true&w=majority`)
+.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kerju.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then(() => {
     app.listen(process.env.PORT || 5000);
 })
