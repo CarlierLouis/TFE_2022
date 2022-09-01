c4558c5b9ed9e6f750bc1c8ace8def39e1da64cf
Louis Carlier <63374771+CarlierLouis@users.noreply.github.com>
2022-09-01 03:04:42 +0200

    2-3 modifs frontend

--- client/src/showcase/Map.js
+++ client/src/showcase/Map.js
@@ -11,7 +11,7 @@ class Map extends Component {
   render() {
     return (
       <LoadScript
-        //googleMapsApiKey="
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
       >
         <GoogleMap
           mapContainerStyle={containerStyle}  
